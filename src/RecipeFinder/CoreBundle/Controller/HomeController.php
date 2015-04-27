8b0002de84c20ce43dd3eb5b91fe2f8e50401e65
Shaunak Deshmukh <sdeshmukh@ljh.com.au>
2015-04-28 07:29:03 +1000

    Added Form and Base classes

--- src/RecipeFinder/CoreBundle/Controller/HomeController.php
+++ src/RecipeFinder/CoreBundle/Controller/HomeController.php
@@ -3,11 +3,33 @@
 namespace RecipeFinder\CoreBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use RecipeFinder\CoreBundle\Form\Type\RecipeFinderType;
+use Symfony\Component\Form\FormError;
+use RecipeFinder\CoreBundle\Common\Item;
+/*
+* Home Controller
+* @author Shaunak Deshmukh
+* @since 1.0
+*/
 
 class HomeController extends Controller
 {
     public function indexAction()
     {
-        return $this->render('RecipeFinderCoreBundle:Home:index.html.twig');
+    	$args = array();
+    	$data = array();	
+
+	    $form = $this->createForm('recipe_finder', $data);    	
+	
+		if($this->getRequest()->isMethod('POST')) {
+		    $form->handleRequest($this->getRequest());   
+
+		    if($form->isValid()) {
+
+		    }
+		}
+
+		$args['form'] = $form->createView(); 
+        return $this->render('RecipeFinderCoreBundle:Home:index.html.twig', $args);
     }
 }
